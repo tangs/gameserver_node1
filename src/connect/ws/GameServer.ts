1fb0b5c6dd5c3af0d444b781fb19e29e12d04606
tangs <tangsong1988kaka@gmail.com>
2019-03-02 23:41:18 +0800

    formatl

--- src/connect/ws/GameServer.ts
+++ src/connect/ws/GameServer.ts
@@ -1,14 +1,13 @@
-'use strict';
-import {Config} from '../Config';
+import { Config } from '../Config';
 import { UserManager } from '../../user/UserManager';
-import {CSProto} from '../../protos/TinyGameCSProto.xml';
-import {MsgBuilder} from '../../protos/csProtoBuilder';
-import {csproto} from '../../protos/KConnectProto.xml';
+// import { CSProto } from '../../protos/TinyGameCSProto.xml';
+import { MsgBuilder } from '../../protos/csProtoBuilder';
+import { csproto } from '../../protos/KConnectProto.xml';
 import { MsgDispatcher } from '../../msg/MsgDispatcher';
 // import { CarServer } from '../../game/CarServer';
 // import { LoginManger } from '../../msg/LoginManger';
 import { Handler } from '../../utils/Handler';
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
 export class GameServer {
     private decodeBuffer: Uint8Array = new Uint8Array(1024 * 20);
@@ -23,7 +22,7 @@ export class GameServer {
         const md: MsgDispatcher = MsgDispatcher.getInstance();
         wss.on('connection', function (ws) {
             let runIntervalId = null;
-            let key: string = "";
+            let key: string = '';
             console.log(`[SERVER] connection()`);
             ws.on('open', function () {
                 console.log(`[CLIENT] open()`);
@@ -35,24 +34,26 @@ export class GameServer {
                 console.log(`[SERVER] Received:` + JSON.stringify(msg));
                 if (msg instanceof csproto.KConnectProto.WX_CMD_NEW_UNIAUTH_CS) {
                     key = msg.IdentityToken;
-                    console.log("222:" + key);
+                    console.log('222:' + key);
                     um.userConnect(key, ws, new Handler(this, (ret) => {
-                        let dest = new csproto.KConnectProto.WX_CMD_NEW_UNIAUTH_SC;
+                        let dest = new csproto.KConnectProto.WX_CMD_NEW_UNIAUTH_SC();
                         dest.RetCode = 0;
-                        dest.RetCodeDesc = "";
-                        dest.SessionId = "111";
+                        dest.RetCodeDesc = '';
+                        dest.SessionId = '111';
                         ws.send(builder.encode(dest));
-                        console.log("ret:" + ret);
+                        console.log('ret:' + ret);
                     }));
                 } else {
                     md.onRecviedMsg(msg, key, ws);
                 }
-            })
-            let leav = () => {
-                if (runIntervalId)
+            });
+            const leav = () => {
+                if (runIntervalId) {
                     clearInterval(runIntervalId);
-                if (key.length > 0) 
+                }
+                if (key.length > 0) {
                     um.userDisConnect(key);
+                }
             };
             ws.on('error', function (error) {
                 console.log(`[CLIENT] error:${error}`);
@@ -64,7 +65,7 @@ export class GameServer {
             });
         });
 
-        console.log("start game server on port:" + (Config.gameServer.port + 20000));
+        console.log('start game server on port:' + (Config.gameServer.port + 20000));
     }
 }
 
