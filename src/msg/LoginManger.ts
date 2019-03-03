216a862111828bed6e9d0d0660ad2864a0b94ae4
tangs <tangsong1988kaka@gmail.com>
2019-03-03 15:19:14 +0800

    format.

--- src/msg/LoginManger.ts
+++ src/msg/LoginManger.ts
@@ -1,11 +1,11 @@
-import { MsgDispatcher } from "./MsgDispatcher";
-import { Handler } from "../utils/Handler";
-import { csprotos } from "../protos/csProtoDecoder";
-import { UserManager } from "../user/UserManager";
-import { MyMsgBuilder } from "./MyMsgBuilder";
-import { CSProto } from "../protos/TinyGameCSProto.xml";
-import { AppEventEmitter } from "../event/AppEventEmitter";
-import { Events } from "../event/Events";
+import { MsgDispatcher } from './MsgDispatcher';
+import { Handler } from '../utils/Handler';
+import { csprotos } from '../protos/csProtoDecoder';
+import { UserManager } from '../user/UserManager';
+import { MyMsgBuilder } from './MyMsgBuilder';
+import { CSProto } from '../protos/TinyGameCSProto.xml';
+import { AppEventEmitter } from '../event/AppEventEmitter';
+import { Events } from '../event/Events';
 
 export class LoginManger {
     private static instance: LoginManger = null;
@@ -21,12 +21,31 @@ export class LoginManger {
         return LoginManger.instance;
     }
 
+    public start(): void {
+        this.md.regeisterMsg('CMD_ALOGIN', new Handler(this, (msg1: csprotos.message, acc: string, ws: any) => {
+            const msg = msg1 as CSProto.CMD_ALOGIN_CS;
+            const dest = new CSProto.CMD_ALOGIN_SC();
+            dest.bResult = 0;
+            ws.send(this.mb.encode(dest));
+            ws.send(this.mb.encode(this.getRoleMiscMsg(acc)));
+            ws.send(this.mb.encode(this.getWarpMsg(msg.iMapTemplateID)));
+            AppEventEmitter.getInstance().emit(Events.ENTER_ROOM, acc, msg.iMapTemplateID);
+        }));
+
+        this.md.regeisterMsg('CMD_PING', new Handler(this, (msg1: csprotos.message, acc: string, ws: any) => {
+            const msg = new CSProto.CMD_PING_SC();
+            msg.dwClientTick = new Date().getTime();
+            ws.send(this.mb.encode(msg));
+        }));
+
+    }
+
     private getWarpMsg(mapId: number = CSProto.MAP_TEMPLATE_ID_HALL): csprotos.message {
         let msg = new CSProto.CMD_WARP_SC();
         msg.wMapID = mapId;
         msg.llServerUnixtimeMs = new Date().getTime();
         return msg;
-    };
+    }
 
     private getRoleMiscMsg(key: string): csprotos.message {
         const info = this.um.getUserInfo(key);
@@ -39,30 +58,12 @@ export class LoginManger {
         const p = new CSProto.PROPERTY();
         p.bPropType = CSProto.LIFEATT_GOLD;
         p.llPropValue = info.coin;
-        att.push(p)
+        att.push(p);
         const p1 = new CSProto.PROPERTY();
         p1.bPropType = CSProto.LIFEATT_VIPLEVEL;
         p1.llPropValue = info.lv;
-        att.push(p1)
+        att.push(p1);
         return msg;
-    };
-
-    public start(): void {
-        this.md.regeisterMsg("CMD_ALOGIN", new Handler(this, (msg1: csprotos.message, acc: string, ws: any) => {
-            const msg = msg1 as CSProto.CMD_ALOGIN_CS;
-            const dest = new CSProto.CMD_ALOGIN_SC();
-            dest.bResult = 0;
-            ws.send(this.mb.encode(dest));
-            ws.send(this.mb.encode(this.getRoleMiscMsg(acc)));
-            ws.send(this.mb.encode(this.getWarpMsg(msg.iMapTemplateID)));
-            AppEventEmitter.getInstance().emit(Events.ENTER_ROOM, acc, msg.iMapTemplateID);
-        }));
-
-        this.md.regeisterMsg("CMD_PING", new Handler(this, (msg1: csprotos.message, acc: string, ws: any) => {
-            const msg = new CSProto.CMD_PING_SC();
-            msg.dwClientTick = new Date().getTime();
-            ws.send(this.mb.encode(msg));
-        }));
-
     }
+
 }
