4326cc189fa9b580dce1f805ed77af8e9283d176
tangs <tangsong1988kaka@gmail.com>
2019-03-03 15:16:56 +0800

    format.

--- src/game/CarServer.ts
+++ src/game/CarServer.ts
@@ -1,10 +1,10 @@
-import { UserManager } from "../user/UserManager";
-import { UserInfo } from "../user/UserInfo";
-import { csprotos } from "../protos/csProtoDecoder";
-import { CSProto } from "../protos/TinyGameCSProto.xml";
-import { MyMsgBuilder } from "../msg/MyMsgBuilder";
-import { AppEventEmitter } from "../event/AppEventEmitter";
-import { Events } from "../event/Events";
+import { UserManager } from '../user/UserManager';
+import { UserInfo } from '../user/UserInfo';
+import { csprotos } from '../protos/csProtoDecoder';
+import { CSProto } from '../protos/TinyGameCSProto.xml';
+import { MyMsgBuilder } from '../msg/MyMsgBuilder';
+import { AppEventEmitter } from '../event/AppEventEmitter';
+import { Events } from '../event/Events';
 
 export class CarServer {
     private static instance: CarServer = null;
@@ -20,6 +20,11 @@ export class CarServer {
 
     private loopId: number = 0;
 
+    constructor() {
+        this.recodes.push(73);
+        this.init();
+    }
+
     public static getInstance(): CarServer {
         if (CarServer.instance == null) {
             CarServer.instance = new CarServer();
@@ -27,10 +32,44 @@ export class CarServer {
         return CarServer.instance;
     }
 
-    constructor() {
-        this.recodes.push(73);
-        this.init();
+    public start(): void {
+        this.roundEnd();
+        this.runId = setInterval(this.roundEnd.bind(this), CarServer.LOOP_TIME);
     }
+
+    public end(): void {
+        if (this.runId) {
+            clearInterval(this.runId);
+        }
+    }
+
+    public userEnter(acc: string): void {
+        const info = this.um.getUserInfo(acc);
+        if (info.isConneted && info.ws) {
+            info.ws.send(this.mb.encode(this.getEnterEndMsg()));
+        }
+        const infos = this.users;
+        let len = infos.length;
+        for (let i = 0; i < len; ++i) {
+            if (info === infos[i]) {
+                return;
+            }
+        }
+        this.users.push(info);
+    }
+
+    public userLeave(acc: string): void {
+        const info = this.um.getUserInfo(acc);
+        const infos = this.users;
+        let len = infos.length;
+        for (let i = 0; i < len; ++i) {
+            if (info === infos[i]) {
+                infos.splice(i, 1);
+                return;
+            }
+        }
+    }
+
 
     private init(): void {
         this.initEvents();
@@ -39,7 +78,7 @@ export class CarServer {
     private initEvents(): void {
         const aee = AppEventEmitter.getInstance();
         aee.on(Events.ENTER_ROOM, (acc: string, mapId: number) => {
-            if (mapId == CSProto.MAP_TEMPLATE_ID_CAR) {
+            if (mapId === CSProto.MAP_TEMPLATE_ID_CAR) {
                 this.userEnter(acc);
             } else {
                 this.userLeave(acc);
@@ -97,42 +136,5 @@ export class CarServer {
             }
         }
         this.lastStartTime = new Date().getTime();
-    }
-
-    public start(): void {
-        this.roundEnd();
-        this.runId = setInterval(this.roundEnd.bind(this), CarServer.LOOP_TIME);
-    }
-
-    public end(): void {
-        if (this.runId)
-            clearInterval(this.runId);
-    }
-
-    public userEnter(acc: string): void {
-        var info = this.um.getUserInfo(acc);
-        if (info.isConneted && info.ws) {
-            info.ws.send(this.mb.encode(this.getEnterEndMsg()));
-        }
-        const infos = this.users;
-        let len = infos.length;
-        for (let i = 0; i < len; ++i) {
-            if (info == infos[i]) {
-                return;
-            }
-        }
-        this.users.push(info);
-    }
-
-    public userLeave(acc: string): void {
-        var info = this.um.getUserInfo(acc);
-        const infos = this.users;
-        let len = infos.length;
-        for (let i = 0; i < len; ++i) {
-            if (info == infos[i]) {
-                infos.splice(i, 1);
-                return;
-            }
-        }
     }
 }
